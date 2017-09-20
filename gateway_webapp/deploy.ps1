dafee47612e2b52ce3d41e9b0e9e2a15e3d6cc99
Michael Friedrich <mifrie@microsoft.com>
2017-09-20 15:51:56 +1000

    added output

--- gateway_webapp/deploy.ps1
+++ gateway_webapp/deploy.ps1
@@ -1,11 +1,8 @@
-#This will deploy a web app with a staging deployment slot
-#You will need to change the repoURL and branch variables to reflect your own 
-
-#Import AzureRM Resource module
+﻿#Import AzureRM Resource module
 Import-Module AzureRM.Resources
 
 #Login
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 #Create random strings for deployment
 $par1 = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
@@ -13,13 +10,12 @@ $par2 = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
 $par3 = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
 
 #GitHub parameters
-$GHTempfile = 'https://raw.githubusercontent.com/fredderf204/ARMTemplates/master/gateway_webapp/azuredeploy.json'
-$repoURL = 'https://github.com/fredderf204/mfnode24.git'
-$branch = 'master'
-$slotname = 'staging'
+$GHTempfile = "https://raw.githubusercontent.com/fredderf204/ARMTemplates/master/gateway_webapp/parent.json"
+$repoURL = "https://github.com/fredderf204/mfnode22.git"
+$appInsights = "no"
 
 #Create resource group
-New-AzureRmResourceGroup -Name $par1 -Location 'Australia East'
+New-AzureRmResourceGroup -Name $par1 -Location "Australia East"
 
 #Deploy azure resources from template
-New-AzureRmResourceGroupDeployment -ResourceGroupName $par1 -TemplateFile $GHTempfile -slotName $slotname -appServicePlanName $par2 -webappname $par3 -repoURL $repoURL -branch $branch -Verbose
+New-AzureRmResourceGroupDeployment -ResourceGroupName $par1 -TemplateFile $GHTempfile -appInsights $appInsights -appServicePlanName $par2 -repoURL $repoURL -webappname $par3 -Verbose
